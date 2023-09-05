66baeb79db1d08465412ac995260776ca4c247b3
keiba-KID <keiba.boc116@gmail.com>
2023-09-05 22:19:40 +1000

    スタイルとルーターを変更する

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <ApiProvider api={apiSlice}>
-      <BrowserRouter>
+      <BrowserRouter basename="movie">
         <AppRoutes></AppRoutes>
       </BrowserRouter>
     </ApiProvider>
